0a1966742101f82409528f7c3b9c85164521a942
Vlad Vinnikov <bigappleinsider@users.noreply.github.com>
2018-06-20 09:02:38 -0700

    initial import

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import axios from "axios";
+import cheerio from "cheerio";
+
+const app = express();
+
+app.get("/tags", async (req, res) => {
+  const { endpoint, tag } = req.query;
+  const { data: pageContent } = await axios.get(endpoint);
+  const $ = cheerio.load(pageContent);
+  const results = [];
+  $(tag).each(function(i, elem) {
+    results.push({
+      innerText: $(this).text(),
+      innerHtml: $(this).html()
+    })
+  });
+  res.send({ tag: results });
+});
+
+app.get("/exists", async (req, res) => {
+  const { endpoint, tag, text } = req.query;
+  const { data: pageContent } = await axios.get(endpoint);
+  const $ = cheerio.load(pageContent);
+  const exists = $(`${tag}:contains(${text})`).length ? true:false;
+  res.send({ exists });
+})
+
+
+app.listen(process.env.PORT || 5000, () => console.log('App listening on port 5000!'))
